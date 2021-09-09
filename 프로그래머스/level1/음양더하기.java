01df3e1b70e64123675b3d88bb8024b83fbd65b0
yoonsung <sbdyzjdla@gmail.com>
2021-09-09 13:28:26 +0900

    음양더하기

---
+++ 프로그래머스/level1/음양더하기.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(int[] absolutes, boolean[] signs) {
+        int answer = 0;
+        for(int i=0; i<absolutes.length; ++i) {
+            if(!signs[i]) {
+                absolutes[i] = (-1) * absolutes[i];
+            }
+            answer += absolutes[i];
+        }
+        return answer;
+    }
+}
