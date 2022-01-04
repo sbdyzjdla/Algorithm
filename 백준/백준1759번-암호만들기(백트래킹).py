e95532a03e17218fb7ee33a5496b92447f321f18
sbdyzjdla <sbdyzjdla@gmail.com>
2022-01-04 10:28:03 +0900

    암호만들기 수정

--- 백준/백준1759번-암호만들기(백트래킹).py
+++ 백준/백준1759번-암호만들기(백트래킹).py
@@ -4,11 +4,13 @@ l, c = map(int, input().split())
 alpha = input().split()
 alpha.sort()
 s = []
-mo = ['a', 'e', 'i', 'o', 'u']
+vowels = set(['a', 'e', 'i', 'o', 'u'])
+
 def main(idx):
     if len(s) == l:
-        print(''.join(map(str, s)))
-        return
+        if len(set(s) & vowels) > 0 and len(set(s) - vowels) > 1:
+            print(''.join(map(str, s)))
+            return
     for i in range(idx, c):
         if i in s:
             continue
